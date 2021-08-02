af91221145fc570246939d38a77ccd5c89c3b3c3
hamza-nasr <68786227+hamza-nasr@users.noreply.github.com>
2021-08-02 17:54:20 -0400

    Add files via upload

--- p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/sketch.js
@@ -16,6 +16,7 @@ var turns
  function setup() {
   createCanvas(480,800);
   createSprite(400, 200, 50, 50);
+  createSprite(240,300,480,10);
   engine = Engine.create();
   world = engine.world;
   Engine.run(engine);
@@ -46,5 +47,14 @@ function draw() {
   background(255,255,255);  
   score(440,750);
   text("100 points", 40, 200, 10, 10 )
+  text("120 points", 80, 200, 10, 100)
+  text("140 points", 120, 200, 10, 10)
+  text("160 points", 160, 200, 10, 10)
+  text("180 points", 200, 200, 10, 10)
+  text("160 points", 240, 200, 10, 10)
+  text("140 points", 280, 200, 10, 10)
+  text("120 points", 320, 200, 10, 10)
+  text("100 points", 360, 200, 10, 10)
+
   drawSprites();
 }
